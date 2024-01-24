92047f804ef4a890a84d8a920a3ce38837a25a6c
claudia93L <34545518+claudia93L@users.noreply.github.com>
2024-01-24 15:01:26 +0100

    anno dinamico sul footer

--- app-job-search/src/components/Footer.jsx
+++ app-job-search/src/components/Footer.jsx
@@ -4,7 +4,10 @@ const Footer = () => {
   return (
     <>
       <Container fluid className='text-center position-fixed bottom-0'>
-        <p>Copyright DreamJob &copy;2024 | All rights reserved </p>
+        <p>
+          Copyright DreamJob &copy; {new Date().getFullYear()} | All rights
+          reserved{' '}
+        </p>
       </Container>
     </>
   );
--- app-meteo/src/components/Footer.jsx
+++ app-meteo/src/components/Footer.jsx
@@ -3,9 +3,15 @@ import { Container } from 'react-bootstrap';
 const Footer = () => {
   return (
     <>
-      <Container fluid className='bg-dark text-white py-3 text-center position-fixed bottom-0'>
-        <p >
-          <small>Copyright &copy; 2024 | IntoTheClouds Weather App</small>
+      <Container
+        fluid
+        className='bg-dark text-white py-3 text-center position-fixed bottom-0'
+      >
+        <p>
+          <small>
+            Copyright &copy; {new Date().getFullYear()} | IntoTheClouds Weather
+            App
+          </small>
         </p>
       </Container>
     </>
