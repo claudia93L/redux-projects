cd73d4a17f66506f536ad70c305636e60ec694aa
claudia93L <34545518+claudia93L@users.noreply.github.com>
2024-01-25 17:34:38 +0100

    rimossa cartella actions dopo upgrade a nuova versione redux-toolkit

--- app-job-search/src/actions/spinnerActions.jsx
+++
@@ -1,9 +0,0 @@
-export const SET_SPINNER = 'SET_SPINNER';
-
-// this will handle the value's update of the spinner
-export const setSpinner = (newState) => {
-  return {
-    type: SET_SPINNER,
-    payload: newState,
-  };
-};
